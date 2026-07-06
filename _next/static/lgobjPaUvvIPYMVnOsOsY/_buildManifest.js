cc71a201d95c08e7325a452380fcf27751709144
testchinesecn <testchinesecn@users.noreply.github.com>
2026-07-06 11:14:16 +0000

    deploy: 1d2f2565a2c06906d3c9565a689b1a0037db6d13

--- _next/static/lgobjPaUvvIPYMVnOsOsY/_buildManifest.js
+++ _next/static/lgobjPaUvvIPYMVnOsOsY/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/web_dmt_hau_viet//_next/:path+",
-        "destination": "/web_dmt_hau_viet/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
