f7ee07b67889b10b87507ecc98eba1059fdbd853
testchinesecn <testchinesecn@users.noreply.github.com>
2026-07-06 07:53:17 +0000

    deploy: 9c165dbfd5d71420660b3f9ff5e40827c444df75

---
+++ _next/static/IuzAA0ENj5pb1BtZGHDVO/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/web_dmt_hau_viet//_next/:path+",
+        "destination": "/web_dmt_hau_viet/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
